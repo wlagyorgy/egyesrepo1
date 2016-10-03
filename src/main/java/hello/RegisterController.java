d2bb57c19f7fb4395c1815435b5c216d8d5a75b4
GergoMuller <bejla94@gmail.com>
2016-10-03 14:38:28 +0200

    init

---
+++ src/main/java/hello/RegisterController.java
@@ -0,0 +1,37 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.jws.WebService;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Controller
+public class RegisterController {
+
+    @Autowired
+    private MyBean bean;
+
+    @GetMapping("/greeting")
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        return "greeting";
+    }
+
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting)
+    {
+        greeting.setContent(bean.modifyName(greeting.getContent()));
+        return "result";
+    }
+
+}
